8e8bf50e24af652f69be45043a8a0dfc25ec820b
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-16 15:19:48 -0700

    Addition of jsx 18 boiler plate and fixed JSX-DOM dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
-
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Create the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the app
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>My Favourite Foods</h1>
+      <ul>
+        <li>Bacon</li>
+        <li>Noodles</li>
+        <li>Jamon</li>
+      </ul>
+    </div>
+  </React.StrictMode>
+);
